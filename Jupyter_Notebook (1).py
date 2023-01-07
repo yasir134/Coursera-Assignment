369780643ddfb5b9cff01238f776b1896de4cc84
yasir134 <122092047+yasir134@users.noreply.github.com>
2023-01-07 15:31:35 +0500

    Add files via upload

---
+++ Jupyter_Notebook (1).py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <center>
+#     <img src="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0105EN-SkillsNetwork/labs/Module2/images/SN_web_lightmode.png" width="300" alt="cognitiveclass.ai logo"  />
+# </center>
+# 
+
+# #### Add your code below following the instructions given in the course
+# 
+
+# <h1>My Juypter Notebook on IBM Watson Studio<h1>
+
+# **Yasir**
+
+# *I am interested in data science because It's a way to get answers, And personaly I consider fixing causes instead of problem, Problem is something happning and could be tackled but if causes identifyed "Chances to face same event twice are less".*
+
+# <h3>Some calculations<h3>
+
+# In[4]:
+
+
+25+35
+
+
+# In[9]:
+
+
+#adding and multiplying
+aNumber = 23
+bNumber = 465
+cNumber = 12
+x = (aNumber+bNumber*cNumber)
+print(x)
+
+
+# In[10]:
+
+
+#Division
+aNumber = 23
+bNumber = 4
+x = (aNumber % bNumber)
+print(x)
+
+
+# <h3>Horizontal rule, Bulleted list, Numbered list & Strikethrough text</h3>
+
+# 1. Pakistan
+# 2. Afghanistan
+# 3. USA
+# 4. China
+# 5. Australia
+
+# * apple
+# * banana
+# * graps
+
+# <del>Data Science is not Science</del>
